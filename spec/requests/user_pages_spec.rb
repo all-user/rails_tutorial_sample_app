e268ad7a74c7008033bd50be0e25e5b030dea3a1
Keita Okamoto <ok@aaa.email.ne.jp>
2015-07-30 09:24:12 +0900

    define spec helper

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,12 +43,7 @@ describe "UserPages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before do fill_valid_infomation end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -56,18 +51,19 @@ describe "UserPages" do
 
       describe "after saving the user" do
         before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+        let(:user) { find_saved_user }
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
 
         it {
           visit user_path(user)
-          should_not have_selector('div.alert.alert-success', text: 'Welcome')
+          should_not have_success_message('Welcome')
         }
       end
     end
+
 
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,25 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def fill_valid_infomation
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def find_saved_user
+  User.find_by(email: 'user@example.com')
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-danger', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
   end
 end
